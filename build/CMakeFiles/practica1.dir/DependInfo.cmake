fd51ecf6d29a78d0078a2ebdad18be5549b35b27
salvacorts <salvacorts97@gmail.com>
2018-04-11 17:54:30 +0200

    Restructure

---
+++ build/CMakeFiles/practica1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/src/greedy.cpp" "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/build/CMakeFiles/practica1.dir/src/greedy.cpp.o"
+  "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/src/localSearch.cpp" "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/build/CMakeFiles/practica1.dir/src/localSearch.cpp.o"
+  "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/src/localSearchDLB.cpp" "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/build/CMakeFiles/practica1.dir/src/localSearchDLB.cpp.o"
+  "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/src/practica1.cpp" "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/build/CMakeFiles/practica1.dir/src/practica1.cpp.o"
+  "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/src/solution.cpp" "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/build/CMakeFiles/practica1.dir/src/solution.cpp.o"
+  "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/src/solver.cpp" "/home/salva/Dropbox/UGR/3/2nd/MH/Practicas/UGR-MH/P1/build/CMakeFiles/practica1.dir/src/solver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
